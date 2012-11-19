84b919a1725cb9af741c3c108bbc1e759ea1aba9
Eb <Ebayle@gmail.com>
2012-11-19 15:30:44 -0500

    Gem Dust Recipies

--- src/common/eb/minecraft/ape/Ape.java
+++ src/common/eb/minecraft/ape/Ape.java
@@ -19,7 +19,6 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.Property;
 
-//TODO: Add recipes for dusts
 
 @Mod(modid="APE", name="Alcohol, Potions, and Enchantments", version="0.0.1")
 @NetworkMod(clientSideRequired=true, serverSideRequired=true)
@@ -70,10 +69,6 @@ public class Ape
 			LanguageRegistry.addName(new ItemStack(mortarAndPestle), "Mortar and Pestle");
 			LanguageRegistry.addName(new ItemStack(gemDust), "Gem Dust");
 			
-			
-			// Adding Recipes
-			GameRegistry.addShapelessRecipe(new ItemStack(Ape.gemDust, 3, 3), new ItemStack(Ape.mortarAndPestle, 1, -1), Item.diamond);
-			
 			// Multi-Item Initialization
 			
 			final String[] dustNames = 
@@ -85,9 +80,38 @@ public class Ape
 			for (int ix = 0; ix < 12; ix++)
 			{
 				ItemStack gemDustIS = new ItemStack(gemDust, 1, ix);
-				
-				//GameRegistry.addShapelessRecipe(gemDustIS, Ape.mortarAndPestle, ("gem" + dustNames[gemDustIS.getItemDamage()].toLowerCase() ));
 				LanguageRegistry.addName(gemDustIS, dustNames[gemDustIS.getItemDamage()] + " Dust");
+			}
+			
+			for (int ix=0; ix < 5; ix++)
+			{
+				ItemStack gemAgateIS = new ItemStack(TFC.TFCItems.GemAgate, 1, ix);
+				ItemStack gemAmethystIS = new ItemStack(TFC.TFCItems.GemAmethyst, 1, ix);
+				ItemStack gemBerylIS = new ItemStack(TFC.TFCItems.GemBeryl, 1, ix);
+				ItemStack gemDiamondIS = new ItemStack(TFC.TFCItems.GemDiamond, 1, ix);
+				ItemStack gemEmeraldIS = new ItemStack(TFC.TFCItems.GemEmerald, 1, ix);
+				ItemStack gemGarnetIS = new ItemStack(TFC.TFCItems.GemGarnet, 1, ix);
+				ItemStack gemJadeIS = new ItemStack(TFC.TFCItems.GemJade, 1, ix);
+				ItemStack gemJasperIS = new ItemStack(TFC.TFCItems.GemJasper, 1, ix);
+				ItemStack gemOpalIS = new ItemStack(TFC.TFCItems.GemOpal, 1, ix);
+				ItemStack gemRubyIS = new ItemStack(TFC.TFCItems.GemRuby, 1, ix);
+				ItemStack gemSapphireIS = new ItemStack(TFC.TFCItems.GemSapphire, 1, ix);
+				ItemStack gemTopazIS = new ItemStack(TFC.TFCItems.GemTopaz, 1, ix);
+				ItemStack gemTourmalineIS = new ItemStack(TFC.TFCItems.GemTourmaline, 1, ix);
+				
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),0)), new ItemStack(Ape.mortarAndPestle,1,-1), gemAgateIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),1)), new ItemStack(Ape.mortarAndPestle,1,-1), gemAmethystIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),2)), new ItemStack(Ape.mortarAndPestle,1,-1), gemBerylIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),3)), new ItemStack(Ape.mortarAndPestle,1,-1), gemDiamondIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),4)), new ItemStack(Ape.mortarAndPestle,1,-1), gemEmeraldIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),5)), new ItemStack(Ape.mortarAndPestle,1,-1), gemGarnetIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),6)), new ItemStack(Ape.mortarAndPestle,1,-1), gemJadeIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),7)), new ItemStack(Ape.mortarAndPestle,1,-1), gemJasperIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),8)), new ItemStack(Ape.mortarAndPestle,1,-1), gemOpalIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),9)), new ItemStack(Ape.mortarAndPestle,1,-1), gemRubyIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),10)), new ItemStack(Ape.mortarAndPestle,1,-1), gemSapphireIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),11)), new ItemStack(Ape.mortarAndPestle,1,-1), gemTopazIS);
+				GameRegistry.addShapelessRecipe((new ItemStack(gemDust,(int)Math.pow(2, ix),12)), new ItemStack(Ape.mortarAndPestle,1,-1), gemTourmalineIS);
 			}
 			
 			proxy.registerRenderers();
